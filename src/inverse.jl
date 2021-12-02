a0b0873f1355825e4812088b967c3d010d810550
Sebastian Ament <sebastianeament@gmail.com>
2021-12-02 13:53:03 -0500

    added support for efficient algebraic operations with inverse of UpdatableCholesky

--- src/inverse.jl
+++ src/inverse.jl
@@ -1,6 +1,6 @@
 # adding Inverse to have efficient ternary dot product and multiply
 # allows for especially efficient Ma
-import LinearAlgebra: dot
+import LinearAlgebra: dot, *, diag
 function dot(x::AbstractVector, A::Inverse{<:Any, <:UpdatableCholesky}, y::AbstractVector)
 	xp = copy(x)
 	yp = x ≡ y ? xp : copy(y)
@@ -11,4 +11,19 @@ import LazyInverses: dot!!, inverse_cholesky_dot!!
 function dot!!(x::AbstractVector, A::Inverse{<:Any, <:UpdatableCholesky}, y::AbstractVector)
 	C = A.parent
 	inverse_cholesky_dot!!(x, C, y)
+end
+
+using LazyInverses: inverse_cholesky_diag, inverse_cholesky_mul, inverse_cholesky_diag_mul
+function diag(A::Inverse{<:Any, <:UpdatableCholesky})
+	inverse_cholesky_diag(A.parent)
+end
+
+import LazyInverses: diag_mul
+function diag_mul(X, A::Inverse{<:Any, <:UpdatableCholesky}, Y)
+	C = A.parent
+	inverse_cholesky_diag_mul(X, C, Y)
+end
+
+function *(X, A::Inverse{<:Any, <:UpdatableCholesky}, Y)
+	inverse_cholesky_mul(X, A.parent, Y)
 end
--- test/inverse.jl
+++ test/inverse.jl
@@ -2,6 +2,7 @@ module TestInverseCholeskyDot
 using LinearAlgebra
 using UpdatableCholeskyFactorizations
 using LazyInverses
+using UpdatableCholeskyFactorizations: diag_mul
 using Test
 
 # testing particularly efficient algebraic operations with inverse(Cholesky)
@@ -9,21 +10,27 @@ using Test
     k = 16
     element_types = (Float32, Float64, ComplexF32, ComplexF64)
     for elty in element_types
-        for n in (16, 1024) # to trigger sequential and parallel code
-            x, y = rand(elty, n), rand(elty, n)
-            X, Y = rand(elty, k, n), rand(elty, n, k)
-            A = randn(elty, n, n)
-            A = A'A + I # for numerical stability, especially for low-precision floats
-            # with UpdatableCholesky
-            C = updatable_cholesky(A)
-            invC = Inverse(C)
-            @test dot(x, invC, x) ≈ dot(x, C\x)
-            @test dot(x, invC, y) ≈ dot(x, C\y)
-            @test *(X, invC, X') ≈ *(X, C\X')
-            @test *(X, invC, Y) ≈ *(X, C\Y)
-            @test diag(invC) ≈ diag(inv(A))
-        end
-    end
+        @testset "eltype $elty" begin
+            for n in (16) #, 1024) # to trigger sequential and TODO: parallel code
+                @testset "size $n" begin
+                    x, y = rand(elty, n), rand(elty, n)
+                    X, Y = rand(elty, k, n), rand(elty, n, k)
+                    A = randn(elty, n, n)
+                    A = A'A + I # for numerical stability, especially for low-precision floats
+                    # with UpdatableCholesky
+                    C = updatable_cholesky(A)
+                    invC = Inverse(C)
+                    @test dot(x, invC, x) ≈ dot(x, C\x)
+                    @test dot(x, invC, y) ≈ dot(x, C\y)
+                    @test *(X, invC, X') ≈ *(X, C\X')
+                    @test *(X, invC, Y) ≈ *(X, C\Y)
+                    @test diag_mul(X, invC, X') ≈ diag(*(X, invC, X'))
+                    @test diag_mul(X, invC, Y) ≈ diag(*(X, invC, Y))
+                    @test diag(invC) ≈ diag(inv(A))
+                end # testset n
+            end # loop over n
+        end # testset eltype
+    end # loop over eltype
 end
 
 end # TestInverseCholeskyDot
